bb9718a1737b4fb4fc98c3336a66b0b175c2cd33
shwetabhardwaj1 <anu2anu@gmail.com>
2025-07-25 19:31:44 +0100

    adding SPN details in main.yaml, changes in yaml

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~> 3.0"
     }
   }
 
@@ -11,7 +11,7 @@ terraform {
     resource_group_name  = "terraform-demo-dev-rg"
     storage_account_name = "tfstatedevdeploy"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key = "${var.environment}.tfstate"
   }
 
 }
@@ -23,6 +23,10 @@ terraform {
 provider "azurerm" {
   //resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,29 @@ variable "storageAccountName" {
   type        = string
   description = "storage Account Name"
 }
+
+variable "client_id" {
+  type        = string
+  description = "Azure Service Principal Client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Service Principal Client Secret"
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment name (e.g., dev, sit, prod)"
+}
