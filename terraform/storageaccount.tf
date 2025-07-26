2f15a022a2ad7ef8716f2f1885cb221c47194533
shwetabhardwaj1 <anu2anu@gmail.com>
2025-07-26 18:04:43 +0100

    pwd current folder

--- terraform/storageaccount.tf
+++ terraform/storageaccount.tf
@@ -9,7 +9,7 @@ resource "azurerm_storage_account" "storageaccountdemo" {
   tags = {
     environment = "development"
     application = "Terraform1Git1HubActions121"
-    managed_by  = "TerraformDemo123456911"
+    managed_by  = "TqerraformDemo123456911"
 
   }
 }
