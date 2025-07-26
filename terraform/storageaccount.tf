572e45d4f88cbec1c65d47dbbfcc247fcb6e075f
shwetabhardwaj1 <anu2anu@gmail.com>
2025-07-26 18:54:07 +0100

    Lock file location print

--- terraform/storageaccount.tf
+++ terraform/storageaccount.tf
@@ -8,7 +8,7 @@ resource "azurerm_storage_account" "storageaccountdemo" {
 
   tags = {
     environment = "development"
-    application = "Terraform1Git1HubActions121"
+    application = "Terraform1Git1HubAc121"
     managed_by  = "T1qerrqaformDemo123456911"
 
   }
