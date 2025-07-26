543533c76b0855ae371fe55e7fedac6d2c9b8bcf
shwetabhardwaj1 <anu2anu@gmail.com>
2025-07-26 18:24:27 +0100

    Pwd check

--- terraform/storageaccount.tf
+++ terraform/storageaccount.tf
@@ -9,7 +9,7 @@ resource "azurerm_storage_account" "storageaccountdemo" {
   tags = {
     environment = "development"
     application = "Terraform1Git1HubActions121"
-    managed_by  = "TqerraformDemo123456911"
+    managed_by  = "TqerrqaformDemo123456911"
 
   }
 }
