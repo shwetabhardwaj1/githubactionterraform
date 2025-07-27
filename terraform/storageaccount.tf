fd9d63ef0a7c426873832c1ef5ed07be08a16823
shwetabhardwaj1 <anu2anu@gmail.com>
2025-07-27 21:02:38 +0100

    Package include

--- terraform/storageaccount.tf
+++ terraform/storageaccount.tf
@@ -8,7 +8,7 @@ resource "azurerm_storage_account" "storageaccountdemo" {
 
   tags = {
     environment = "development"
-    application = "Terform1Git1HubAc121"
+    application = "Terrform1Git1HubAc121"
     managed_by  = "Te123456911"
 
   }
