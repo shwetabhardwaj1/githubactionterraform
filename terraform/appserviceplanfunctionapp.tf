06d76ef0487c73bc04b7cb11f93ec0e3d6e2f9a9
shwetabhardwaj1 <anu2anu@gmail.com>
2025-07-27 13:49:24 +0100

    dotnet-function-plan-demo1

--- terraform/appserviceplanfunctionapp.tf
+++ terraform/appserviceplanfunctionapp.tf
@@ -1,9 +1,9 @@
 # App Service Plan (Consumption - Windows)
 resource "azurerm_service_plan" "function_plan" {
-  name                = "dotnet-function-plan-demo"
+  name                = "dotnet-function-plan-demo1"
   location            = "UK South"
   resource_group_name = data.azurerm_resource_group.rg.name
   os_type             = "Windows"
-  sku_name            = "F1"  
+  sku_name            = "B1"  
   worker_count = 1
 }
