204efbb8fe83fa01cc920d3791bb711b6d5c4920
shwetabhardwaj1 <anu2anu@gmail.com>
2025-07-27 12:31:20 +0100

    azurerm_service_plan

--- terraform/appserviceplanfunctionapp.tf
+++ terraform/appserviceplanfunctionapp.tf
@@ -1,11 +1,8 @@
 # App Service Plan (Consumption - Windows)
-resource "azurerm_app_service_plan" "function_plan" {
+resource "azurerm_service_plan" "function_plan" {
   name                = "dotnet-function-plan-demo"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  kind                = "FunctionApp"
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  os_type             = "Windows"
+  sku_name            = "Y1"  # This corresponds to the Consumption plan
 }
