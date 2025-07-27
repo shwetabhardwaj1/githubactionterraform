109b6119c9b83ba241169985de7bae301316f33c
shwetabhardwaj1 <anu2anu@gmail.com>
2025-07-27 12:45:00 +0100

    updated latest function appA

--- terraform/functionapp.tf
+++ terraform/functionapp.tf
@@ -1,19 +1,19 @@
-# Function App with Managed Identity
-resource "azurerm_function_app" "demo_func" {
-  name                = "dotnet-function-tf-demo"
+# Window Function App with Managed Identity
+resource "azurerm_windows_function_app" "demo_func" {
+  name                = "my-windows-function-app-demo"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_service_plan.function_plan.id
-  storage_account_name = azurerm_storage_account.storageaccountdemo
+  service_plan_id            = azurerm_service_plan.function_plan.id
+  storage_account_name = azurerm_storage_account.storageaccountdemo.name
   storage_account_access_key = azurerm_storage_account.storageaccountdemo.primary_access_key
 
-  version = "~4"  # Targets .NET 6+
-
-  os_type = "Windows"
-
-  site_config {
-    ftps_state = "Disabled"
+  functions_extension_version = "~4"  # Targets .NET 6+
+ site_config {
+    application_stack {
+      dotnet_version = "6.0"  # Update to your desired .NET version
+    }
   }
+
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME"     = "dotnet"
@@ -27,7 +27,7 @@ resource "azurerm_function_app" "demo_func" {
 
 # Role Assignment: Allow Function App to access Storage
 resource "azurerm_role_assignment" "storage_contributor" {
-  scope                = azurerm_storage_account.storage.id
+  scope                = azurerm_storage_account.storageaccountdemo.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_function_app.demo_func.identity.principal_id
 }
