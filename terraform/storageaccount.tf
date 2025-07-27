24a9c9344b755218621831bf35deaf79c2601bff
shwetabhardwaj1 <anu2anu@gmail.com>
2025-07-27 20:22:06 +0100

    tag for storage account

--- terraform/storageaccount.tf
+++ terraform/storageaccount.tf
@@ -9,7 +9,7 @@ resource "azurerm_storage_account" "storageaccountdemo" {
   tags = {
     environment = "development"
     application = "Terform1Git1HubAc121"
-    managed_by  = "Tsme123456911"
+    managed_by  = "Te123456911"
 
   }
 }
